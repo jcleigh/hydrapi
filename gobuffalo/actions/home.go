b3a25b5814a46fba7861dcdee54c5bbf89423ec5
Jordan Cleigh <cleigh.jordan@gmail.com>
2022-09-25 21:14:15 -0500

    Adds go/buffalo container

---
+++ gobuffalo/actions/home.go
@@ -0,0 +1,17 @@
+package actions
+
+import (
+	"net/http"
+
+	"github.com/gobuffalo/buffalo"
+)
+
+// HomeHandler is a default handler to serve up
+// a home page.
+func HomeHandler(c buffalo.Context) error {
+	return c.Render(http.StatusOK, r.String("go/buffalo"))
+}
+
+func HelloWorldHandler(c buffalo.Context) error {
+	return c.Render(http.StatusOK, r.String("Hello, World!"))
+}
